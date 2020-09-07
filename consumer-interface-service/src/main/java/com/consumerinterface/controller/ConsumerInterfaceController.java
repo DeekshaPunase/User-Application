a5676df32110a838338f87430c77885edb2bb0ea
DeekshaPunase <deeksha.punase08@gmail.com>
2020-09-07 18:04:00 +0530

    User Input Interface and interacts with other microservices through queue

--- consumer-interface-service/src/main/java/com/consumerinterface/controller/ConsumerInterfaceController.java
+++ consumer-interface-service/src/main/java/com/consumerinterface/controller/ConsumerInterfaceController.java
@@ -1,17 +1,14 @@
 package com.consumerinterface.controller;
 
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Pattern;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
